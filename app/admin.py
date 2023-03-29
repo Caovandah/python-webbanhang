b8aa682ce186af032eb777f5a56336bf1418c6be
caovandanh <caovandanh2002@gmail.com>
2023-03-29 21:56:37 +0700

    Upload: Tạo một web bán hàng online từ A tới B #8

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Puoduct)
+admin.site.register(Oder)
+admin.site.register(OderTtem)
+admin.site.register(ShippingAddress)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.SET_NULL, null=True, blank=False)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Puoduct(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Oder(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    dete_oder = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OderTtem(models.Model):
+    product = models.ForeignKey(
+        Puoduct, on_delete=models.SET_NULL, blank=True, null=True)
+    oder = models.ForeignKey(
+        Oder, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    oder = models.ForeignKey(
+        Oder, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    cyti = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    mobile = models.CharField(max_length=10, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
