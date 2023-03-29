25a00573f661e2cf11b0beefcd7d0e7807d04786
caovandanh <caovandanh2002@gmail.com>
2023-03-29 22:17:55 +0700

    Upload: Tạo một web bán hàng online từ A tới B #9

--- app/views.py
+++ app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+
+
 def home(request):
-    context= {}
-    return render(request,'app/home.html',context)
+    products = Puoduct.objects.all()
+    context = {'products': products}
+    return render(request, 'app/home.html', context)
+
+
 def cart(request):
-    context= {}
-    return render(request, 'app/cart.html',context)
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+
 def checkout(request):
-    context= {}
-    return render(request, 'app/checkout.html',context)
+    context = {}
+    return render(request, 'app/checkout.html', context)
