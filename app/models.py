54112086998ef8a77f9a6c406ff5df7162961987
caovandanh <caovandanh2002@gmail.com>
2023-03-30 22:59:42 +0700

    Upload: Tạo một web bán hàng online từ A tới B #11

--- app/models.py
+++ app/models.py
@@ -32,31 +32,48 @@ class Puoduct(models.Model):
         return url
 
 
-class Oder(models.Model):
+class Order(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    dete_oder = models.DateTimeField(auto_now_add=True)
+    dete_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
-class OderTtem(models.Model):
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+
+class OrderItem(models.Model):
     product = models.ForeignKey(
         Puoduct, on_delete=models.SET_NULL, blank=True, null=True)
-    oder = models.ForeignKey(
-        Oder, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    oder = models.ForeignKey(
-        Oder, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     cyti = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
