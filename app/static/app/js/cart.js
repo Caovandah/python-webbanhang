eb09d543d29fa5cce3aef8e7cc63762d312b5e80
caovandanh <caovandanh2002@gmail.com>
2023-03-31 23:21:42 +0700

    Upload: Tạo một web bán hàng online từ A tới B #14

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -9,7 +9,26 @@ for (i=0;i < updateBtns.length;i++){
         if (user === "AnonymousUser"){
             console.log('user not logged-in')
         }else {
-            console.log('user logged-in, success add')
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('user logged-in, success add')
+    var url = '/update_item/'
+    fetch(url,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId':productId,'action':action})
+    })
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        console.log('data',data)
     })
 }
