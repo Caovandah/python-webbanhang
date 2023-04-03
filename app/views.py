0434351f29b0cc2b136a6727e60eca2e792a5e48
caovandanh <caovandanh2002@gmail.com>
2023-04-03 17:32:13 +0700

    Upload: Tạo một web bán hàng online từ A tới B #18

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import *
 import json
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -84,6 +86,22 @@ def register(request):
     return render(request, 'app/register.html', context)
 
 
-def login(request):
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'User or password not correct!')
     context = {}
     return render(request, 'app/login.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
