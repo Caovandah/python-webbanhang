f4f2b7a778bb3edbf55d21efe5d613cc647ead7d
caovandanh <caovandanh2002@gmail.com>
2023-04-03 16:56:57 +0700

    Upload: Tạo một web bán hàng online từ A tới B #17

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your models here.
 
@@ -82,3 +83,10 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.address
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name',
+                  'last_name', 'password1', 'password2']
